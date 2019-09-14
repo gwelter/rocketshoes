745788563be8cde5ea5810931f9870b635b55902
Guilherme Welter <guilherme.a.welter@gmail.com>
2019-09-14 15:10:21 -0300

    Using double quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,29 +3,29 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: ['airbnb', 'prettier', 'prettier/react'],
+  extends: ["airbnb", "prettier", "prettier/react"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-  parser: 'babel-eslint',
+  parser: "babel-eslint",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: ['react', 'react/prettier'],
+  plugins: ["react", "react/prettier"],
   rules: {
-    'prettier/prettier': 'error',
-    'react/jsx-filename-extension': [
-      'warn',
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": [
+      "warn",
       {
-        extensions: ['.jsx', 'js'],
+        extensions: [".jsx", "js"],
       },
     ],
-    'import/prefer-default-export': 'off',
-    'react/state-in-constructor': 'off',
+    "import/prefer-default-export": "off",
+    "react/state-in-constructor": "off",
   },
 };
